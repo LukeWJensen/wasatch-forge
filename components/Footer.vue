0fba8446305bb4c1605202785974825dc715ca88
Luke Jensen <luke@welikesmall.com>
2019-05-01 01:10:26 -0600

    History page updates copy, images, styles

--- components/Footer.vue
+++ components/Footer.vue
@@ -4,8 +4,8 @@
 
             <nav class="main-footer-nav">
                 <nuxt-link class="main-footer-nav-link" to="/shop">The Shop</nuxt-link>
-                <nuxt-link class="main-footer-nav-link" to="/history">Our History</nuxt-link>
                 <nuxt-link class="main-footer-nav-link" to="/blacksmiths">The Blacksmiths</nuxt-link>
+                <nuxt-link class="main-footer-nav-link" to="/history">Our History</nuxt-link>
                 <nuxt-link class="main-footer-nav-link" to="/products">Products</nuxt-link>
                 <nuxt-link class="main-footer-nav-link" to="/contact">Contact</nuxt-link>
             </nav>
